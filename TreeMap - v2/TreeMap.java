4d92c1b66b9e27dcebd327ecb937be287f5747f1
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-18 22:54:23 -0600

    limpieza del codigo

--- TreeMap - v2/TreeMap.java
+++ TreeMap - v2/TreeMap.java
@@ -20,15 +20,8 @@ public class TreeMap {
 	Metodo que procesa el arbol para calcular los tamanos y posiciones
 	*/
 	public void procArbol(Nodo nodo, int x, int y, int largo, int ancho, boolean modificarX){
-		System.out.println("Entro nodo: " + nodo.nombre);
-		System.out.println("Entro x: " + x);
-		System.out.println("Entro y: " + y);
-		System.out.println("Entro largo: " + largo);
-		System.out.println("Entro ancho: " + ancho);
-		System.out.println("Entro modificarX: " + modificarX);
 		if(nodo.hijos == null){
 			interfaz.dibujarPanel(nodo.nombre, x, y, largo, ancho);
-			System.out.println("Llegue a la hoja: " + nodo.nombre);
 		}else{
 			ArrayList<Nodo> hijosNodo = nodo.hijos;
 			int nuevoX = x;
@@ -38,15 +31,11 @@ public class TreeMap {
 			for(int i = 0; i < hijosNodo.size(); i++){
 				Nodo nodoHijo = hijosNodo.get(i);
 				if(modificarX){
-					System.out.println("1Envio a " + nodoHijo.nombre);
-					System.out.println("1Envio porcentaje: " + nodoHijo.getPorcentaje());
-					nuevoLargo = (int)( ((double)largo /*- (double)x*/) * nodoHijo.getPorcentaje() );
+					nuevoLargo = (int)( ((double)largo) * nodoHijo.getPorcentaje() );
 					procArbol( nodoHijo , nuevoX, nuevoY, nuevoLargo, nuevoAncho, !modificarX);
 					nuevoX += nuevoLargo;
 				}else{
-					System.out.println("2Envio a " + nodoHijo.nombre);
-					System.out.println("2Envio porcentaje: " + nodoHijo.getPorcentaje());
-					nuevoAncho = (int)( ((double)ancho /*- (double)y*/) * nodoHijo.getPorcentaje() );
+					nuevoAncho = (int)( ((double)ancho) * nodoHijo.getPorcentaje() );
 					procArbol( nodoHijo , nuevoX, nuevoY, nuevoLargo, nuevoAncho, !modificarX);
 					nuevoY += nuevoAncho;
 				}
